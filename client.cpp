39f4b7969ea7931bc78b3c8b99bc17d22ea7ff86
Arthur Saito <arthur_saito1998@hotmail.com>
2024-05-30 22:17:47 -0300

    +client

--- client.cpp
+++ client.cpp
@@ -3,3 +3,49 @@
 //
 
 #include "client.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include "sys/socket.h"
+#include <netinet/ip.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+static void die(const char *msg) {
+    int err = errno;
+    fprintf(stderr, "[%d] %s\n", err, msg);
+    abort();
+}
+
+int main(){
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(fd < 0){
+        die("socket()");
+    }
+
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = ntohs(1234);
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);
+
+    int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if(rv){
+        die("connect");
+    }
+
+    char msg[] = "hello";
+    write(fd, msg, strlen(msg))
+
+    char rbuff[64] = {};
+    ssize_t n = read(fd, rbuff, sizeof(rbuff) - 1);
+    if(n < 0){
+        die("read");
+    }
+
+    printf("server response: %s\n", rbuff);
+    close(fd);
+    return 0;
+}
--- server.cpp
+++ server.cpp
@@ -3,11 +3,15 @@
 //
 
 #include "server.h"
+#include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 #include <string.h>
 #include "sys/socket.h"
 #include <netinet/ip.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 
 static void die(const char *msg) {
