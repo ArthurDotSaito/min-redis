b5e992d69d210ab1b9798f646d3800f06f02f97b
Arthur Saito <arthur_saito1998@hotmail.com>
2024-07-10 00:07:40 -0300

    ++handle get command

--- libs/common.h
+++ libs/common.h
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <assert.h>
 
 const size_t k_max_msg = 4096;
 
--- server/commands.cpp
+++ server/commands.cpp
@@ -4,5 +4,21 @@
 
 #include <commands.h>
 #include <string>
+#include <vector>
 
 std::map<std::string, std::string> g_map;
+
+uint32_t handle_get(const std::vector<std::string> &command, uint8_t *res, uint32_t *reslen)
+{
+    if (!g_map.count(command[1]))
+    {
+        return RES_NX;
+    }
+    std::string &val = g_map[command[1]];
+    assert(val.size() <= k_max_msg);
+
+    memcpy(res, val.data(), val.size());
+    *reslen = (uint32_t)val.size();
+
+    return RES_OK;
+}
